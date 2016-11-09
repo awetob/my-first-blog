fea174c0cf765c8f044f0b7307c6076105ad5e24
tobembem <aweoluwatobi26@gmail.com>
2016-11-09 13:54:42 +0000

    new files

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-<<<<<<< HEAD
-]
-=======
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name = 'post_detail'),
 ]
->>>>>>> e5c513e83a95d223e9e20dd26b9f80dc8e000af3
--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,9 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-<<<<<<< HEAD
-    return render(request, 'blog/post_list.html', {'posts': posts})
-=======
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
->>>>>>> e5c513e83a95d223e9e20dd26b9f80dc8e000af3
+
